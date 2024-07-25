262b7d5e5249344189d86fe3f41d1dbd34880ce1
Geo <gtip22@gmail.com>
2024-07-25 14:19:47 -0600

    zap_scan init

---
+++ zap_scan.py
@@ -0,0 +1,32 @@
+import time
+import os
+from zapv2 import ZAPv2
+
+target = 'http://juice-shop:3000'
+api_key = os.getenv('ZAP_API_KEY')
+
+zap = ZAPv2(apikey=api_key, proxies={'http': 'http://localhost:8080', 'https': 'http://localhost:8080'})
+
+print(f'Accessing target {target}')
+zap.urlopen(target)
+time.sleep(2)
+
+print('Spidering target')
+scan_id = zap.spider.scan(target)
+time.sleep(2)
+while int(zap.spider.status(scan_id)) < 100:
+    print(f'Spider progress %: {zap.spider.status(scan_id)}')
+    time.sleep(2)
+
+print('Spider completed')
+print('Scanning target')
+scan_id = zap.ascan.scan(target)
+while int(zap.ascan.status(scan_id)) < 100:
+    print(f'Scan progress %: {zap.ascan.status(scan_id)}')
+    time.sleep(5)
+
+print('Scan completed')
+print('Alerts:')
+alerts = zap.core.alerts(baseurl=target)
+for alert in alerts:
+    print(alert)
